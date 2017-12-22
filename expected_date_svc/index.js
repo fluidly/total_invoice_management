4da36a65e24131e2a55da9bb0a5029bdcbdbcc74
Dom Barker <dom.barker808@gmail.com>
2017-12-22 12:26:34 +0000

    add it all

---
+++ expected_date_svc/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const moment = require("moment")
+
+const app = express()
+
+app.get("/api/expected-date/:invoiceId", (req, res) => {
+  const invoiceId = parseInt(req.params.invoiceId)
+
+  // ¯\_(ツ)_/¯
+  const bump = Math.floor(Math.random() * 10) + 1
+
+  const expectedDate = moment().add(bump, 'days').toISOString()
+
+  res.json({
+    invoiceId,
+    expectedDate
+  })
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+  console.log(`invoices_svc listening on ${port}`)
+})
