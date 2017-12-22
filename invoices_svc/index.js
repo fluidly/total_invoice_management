5c1f5493ebffe036641d8a757582141a71702d77
Dom Barker <dom.barker808@gmail.com>
2017-12-22 12:23:15 +0000

    fin

--- invoices_svc/index.js
+++ invoices_svc/index.js
@@ -1,16 +1,34 @@
 const express = require("express")
+const request = require("request-promise")
 
 const app = express()
 
-app.get("/api/invoices/:id", (req, res) => {
-  const id = parseInt(req.params.id)
-  res.json({
-    id: id,
-    ref: `INV-${id}`,
-    amount: id * 100,
-    balance: (id * 100) - 10,
-    ccy: "GBP"
-  })
+const addExpectedDate = async invoice => {
+  try {
+    const { expectedDate } = await request(`${process.env.EXPECTED_DATE_URI}/api/expected-date/${invoice.id}`, {
+      json: true
+    })
+    return Object.assign({}, invoice, { expectedDate })
+  } catch (e) {
+    console.log(`failed to add expected date ${e}`)
+    return invoice
+  }
+}
+
+app.get("/api/invoices/:id", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id)
+    const invoice = await addExpectedDate({
+      id: id,
+      ref: `INV-${id}`,
+      amount: id * 100,
+      balance: (id * 100) - 10,
+      ccy: "GBP"
+    })
+    res.json(invoice)
+  } catch (error) {
+    next(error)
+  }
 })
 
 const port = process.env.PORT || 8080
